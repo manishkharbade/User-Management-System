363adb576cb04cc6f09ba0fc95b40901d7df4785
Manish Kharbade <manishkharbade@gmail.com>
2023-02-02 19:34:06 +0530

    third commit

--- usermanagementsystem/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div className="text-center p-4 copyrightsec bg-light">
-      © 2023 Copyright: @ Techademy
-    </div>
-  );
-}
